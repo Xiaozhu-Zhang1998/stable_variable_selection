376ae818995da48e2238d4d9051ed1862797655e
Xiaozhu Zhang <xzzhang1998@gmail.com>
2022-04-11 23:43:48 -0400

    fixed bugs

--- MultipleSolution/sample_beta.R
+++ MultipleSolution/sample_beta.R
@@ -26,33 +26,45 @@ sample_beta = function(type, X, beta, y = NULL, lambda = NULL, npoints = 1000, w
     stop("The type is invalid!")
   }
   
+  # check whether all elements of ind are 0
+  if(length(ind) == 0) {
+    return(matrix(1, nrow = npoints, ncol = 1) %*% t(as.matrix(beta_store)))
+  }
+  
   # subset according to equi-correlation / gen_inv index
-  X = X[, ind]
+  X = as.matrix(X[, ind])
   beta = beta[ind]
+  
+  unique = FALSE
   
   # first make several pilot draws
   tryCatch({
     pilot_soln = pilot_draws(X, beta, npoints = 1000)
     },
   error = function(e) {
-    print("The LASSO solution is unique.")}
-  )
-  
-  # find the orthant the draws
-  orthant = find_orthant(pilot_soln)
-  
-  # find a x_par that in the orthant
-  x_par = find_x_par(pilot_soln, orthant)
-  
-  # make "target draws"
-  tryCatch({
-    soln = target_draws(X, beta, orthant, x_par, npoints, walk, walk_length)
-    },
-  error = function(e) {
-    soln <<- matrix(1, nrow = npoints, ncol = 1) %*% t(as.matrix(beta))
     print("The LASSO solution is unique.")
+    soln <<- matrix(1, nrow = npoints, ncol = 1) %*% t(as.matrix(beta))
+    unique <<- TRUE
     }
   )
+  
+  if(unique == FALSE) {
+    # find the orthant the draws
+    orthant = find_orthant(pilot_soln)
+    
+    # find a x_par that in the orthant
+    x_par = find_x_par(pilot_soln, orthant)
+    
+    # make "target draws"
+    tryCatch({
+      soln = target_draws(X, orthant, x_par, npoints, walk, walk_length = walk_length)
+    },
+    error = function(e) {
+      soln <<- matrix(1, nrow = npoints, ncol = 1) %*% t(as.matrix(beta))
+      print("The LASSO solution is unique.")
+      }
+    )
+  }
   
   # add back the fixed columns
   result = matrix(1, nrow = npoints, ncol = 1) %*% t(as.matrix(beta_store))
