615bd6af511ced0d49295815e62d2dd51ebf488a
Xiaozhu Zhang <xzzhang1998@gmail.com>
2022-08-23 09:58:25 -0400

    find \xi

--- MultipleSolution/facial_reduction.R
+++ MultipleSolution/facial_reduction.R
@@ -8,37 +8,38 @@ facial_reduction = function(X, beta, tol2) {
   n = nrow(A)
   
   # find the y
-  y_par = c(rep(1, n-1), - sum(b[1:(n-1)]) / b[n])
-  V = pracma::nullspace(t(b))
-  
-  A_ = - t(A) %*% V
-  b_ = t(A) %*% y_par
-  
-  flag = FALSE
-  while(TRUE) {
-    # try-catch
-    tryCatch({
-      P = volesti::Hpolytope(A = A_, b = as.vector(b_) + abs(rnorm(2 * d, sd = 1e-10)))
-      random_walk = list(
-        walk = 'BiW',
-        walk_length = 1
-      )
-      alpha = volesti::sample_points(P, n = 100, random_walk = random_walk)
-    },
-    error = function(e) {
-      flag <<- TRUE
-    })
-    # flag
-    if(!flag) {
+  for(i in rev(1:n)) {
+    # try yi = 1
+    e = rep(0, n)
+    e[i] = 1
+    f.obj = rep(1, nrow(A))
+    f.con = rbind(t(A), t(b), t(e))
+    f.dir = c(rep(">=", ncol(A)), "==", "==")
+    f.rhs = c(rep(0, ncol(A) + 1), 1)
+    op = lpSolve::lp("min", f.obj, f.con, f.dir, f.rhs)
+    y = op$solution
+    
+    # find the FR
+    z = t(A) %*% y
+    sz = z > tol2
+    if(sum(sz) != 0) {
+      break
+    }
+    
+    # try yi = -1
+    e[i] = -1
+    f.con = rbind(t(A), t(b), t(e))
+    op = lpSolve::lp("min", f.obj, f.con, f.dir, f.rhs)
+    y = op$solution
+    
+    # find the FR
+    z = t(A) %*% y
+    sz = z > tol2
+    if(sum(sz) != 0) {
       break
     }
   }
   
-  y = y_par + V %*% alpha[, 100]
-  
-  # find the FR
-  z = t(A) %*% y
-  sz = z > tol2
   FR = diag(rep(1, 2 * d))[, !sz]
   
   return(list(A = A, b = b, FR = FR))
